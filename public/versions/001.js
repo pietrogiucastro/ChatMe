710ae6d2d90785ea773ac2d7684d70ad9701bd54
Pietro Giucastro <Omi016@omigrade.local>
2017-10-20 11:39:36 +0200

    trying again with ga

--- public/versions/001.js
+++ public/versions/001.js
@@ -2041,11 +2041,11 @@ setInterval(function() {
 	$('.pm-messages').children('.pm-message').each(updateDate);
 }, 30000);
 
-/*!function trigger() {
+!function trigger() {
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-108346079-1', 'auto');
 ga('send', 'pageview', site);
-}();*/
+}();
