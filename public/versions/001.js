3f0f414fe57cfb5dbd5a323505f58db9a39960a5
Pietro Giucastro <Omi016@omigrade.local>
2017-10-20 11:21:59 +0200

    added new analytics tracking id

--- public/versions/001.js
+++ public/versions/001.js
@@ -2042,10 +2042,10 @@ setInterval(function() {
 }, 30000);
 
 !function trigger() {
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-105840630-1', 'auto');
-  ga('send', 'pageview', site);
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-108346079-1', 'auto');
+ga('send', 'pageview', site);
 }();
